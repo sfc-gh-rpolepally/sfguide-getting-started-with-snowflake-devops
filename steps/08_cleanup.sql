d8b06d786ced2b9c89007cbde29d86bd1fd5c7f9
Ranga Polepally <ranga.polepally@snowflake.com>
2024-10-01 10:48:36 -0500

    Update 08_cleanup.sql

--- steps/08_cleanup.sql
+++ steps/08_cleanup.sql
@@ -1,7 +1,7 @@
 use role accountadmin;
 drop warehouse if exists quickstart_wh;
-drop database if exists quickstart_prod;
-drop database if exists quickstart_dev;
-drop database if exists quickstart_common;
+drop database if exists prod_db;
+drop database if exists dev_db;
+drop database if exists common_db;
 drop integration if exists git_api_integration;
 drop integration if exists email_integration;
