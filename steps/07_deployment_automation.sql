f25e64c450375adbe7e8dc066d5cb355a61eae87
Ranga Polepally <ranga.polepally@snowflake.com>
2024-10-01 10:47:14 -0500

    Update 07_deployment_automation.sql

--- steps/07_deployment_automation.sql
+++ steps/07_deployment_automation.sql
@@ -8,6 +8,6 @@ This will start the preconfigured CI/CD pipeline and deploy the changes
 use role accountadmin;
 
 -- should return 0 (due to deploy_pipeline_dev.yml)
-show parameters like 'data_retention_time_in_days' in table quickstart_dev.gold.vacation_spots;
+show parameters like 'data_retention_time_in_days' in table dev_db.gold.vacation_spots;
 -- should return 1 (due to deploy_pipeline_dev.yml)
-show parameters like 'data_retention_time_in_days' in table quickstart_prod.gold.vacation_spots;
+show parameters like 'data_retention_time_in_days' in table prod_db.gold.vacation_spots;
